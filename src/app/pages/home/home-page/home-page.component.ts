f2706a59696da7728997b7c37e2c6faf0c527d4f
AmmarBarish <bareesh.ammar00@gmail.com>
2024-01-31 17:15:50 +0400

    Ammar fix arabic

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -2,12 +2,13 @@ import { Component, OnDestroy } from '@angular/core';
 import { ComponentsModule } from '../../../components/components.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ComponentsModule,TranslateModule,NgOptimizedImage],
+  imports: [ComponentsModule,TranslateModule,NgOptimizedImage,RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
