eb0298d8350ca44ecf3865f0fd82ea83ca108b0e
AmmarBarish <bareesh.ammar00@gmail.com>
2024-04-08 06:35:17 +0400

    Fix Some issue in trustpilot style

--- src/app/components/global/trustpilot/trustpilot.component.ts
+++ src/app/components/global/trustpilot/trustpilot.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, OnDestroy, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-trustpilot',
@@ -6,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './trustpilot.component.html',
   styleUrl: './trustpilot.component.scss'
 })
-export class TrustpilotComponent {
+export class TrustpilotComponent implements OnInit, OnDestroy{
+  private scriptElement!: HTMLScriptElement;
+
+  constructor(private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadTrustpilotWidget();
+    }
+  }
+
+  loadTrustpilotWidget(): void {
+    this.scriptElement = this.renderer.createElement('script');
+    this.scriptElement.type = 'text/javascript';
+    this.scriptElement.src = '//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js';
+    this.scriptElement.defer = true;
+    this.renderer.appendChild(document.body, this.scriptElement);
+  }
+
+  ngOnDestroy(): void {
+
+      if (isPlatformBrowser(this.platformId) && this.scriptElement) {
+        this.renderer.removeChild(document.body, this.scriptElement);
+      }
+  }
 
 }
